b1b28aee11683f80f11a8599c0795b86b9acd472
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2024-08-15 23:10:03 -0300

    feat: added react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export {}
+import { Route, Routes, Navigate } from "react-router-dom"
+
+//I named it AppRoutes to avoid conflict with new features of react-router-dom-v6
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path='/initial-page' element={<p>initial page</p>} />
+      <Route path='*' element={<Navigate to='/initial-page' />} />
+    </Routes>
+  )
+}
