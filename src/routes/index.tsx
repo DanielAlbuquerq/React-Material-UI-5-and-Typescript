18aeaae5a332947a2598f9ad4acedd8d83d5c26e
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2024-08-31 13:50:26 -0300

    first commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@mui/material"
 import { Route, Routes, Navigate } from "react-router-dom"
 
 //I named it AppRoutes to avoid conflict with new features of react-router-dom-v6
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path='/initial-page' element={<p>initial page</p>} />
+      <Route path='/initial-page' element={<Button>Click here</Button>} />
       <Route path='*' element={<Navigate to='/initial-page' />} />
     </Routes>
   )
---
+++ src/themes/Light.ts
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material"
+import { yellow } from "@mui/material/colors"
+
+export const LightTheme = createTheme({
+  palette: {
+    primary: {
+      main: yellow[700],
+      dark: yellow[800],
+      light: yellow[700],
+      contrastText: #ffffff,
+    },
+    secondary: {
+      main: yellow[700],
+      dark: yellow[800],
+      light: yellow[700],
+      contrastText: #ffffff,
+    },
+  },
+})
